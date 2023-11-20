0aa069c785e1e9fd4d6f37d1273adbb60ad6a7f5
tristankahl <tristankahl@gkmedien.com>
2023-11-20 21:49:20 +0100

    added momentum plot

--- main.jl
+++ main.jl
@@ -8,6 +8,10 @@ savefig("fig/blocks.pdf")
 
 r, p = simulate_analytic([2.0, 3.0], [0.0, -1.0], [1.0, 100.0], 5000, 0.1)
 
-plot(r[:,1], label="r1", color="blue")
-plot!(r[:,2], label="r2", color="brown")
+plot(r[:,1], label="\$r_1\$", color="blue")
+plot!(r[:,2], label="\$r_2\$", color="brown")
 savefig("fig/spacetime.pdf")
+
+plot(p[:, 2] / 10, p[:, 1], xlabel="\$p_2 / \\sqrt{m_2}\$", ylabel="\$p_1 / \\sqrt{m_1}\$", aspect_ratio=:equal, label="")
+plot!([0.1 * cos(x) for x in 0.0:0.01:2*pi], [0.1 * sin(x) for x in 0.0:0.01:2*pi], label="")
+savefig("fig/p1p2.pdf")
